a72ef4b4d833f7dce88409e4c04d8322b4d38b5c
Felix Deimel <felix@lemonmojo.com>
2023-12-07 10:38:04 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿foreach (var type in System.Reflection.Assembly.GetExecutingAssembly().ExportedTypes) {
+	Console.WriteLine($"{type.Name}");
+}
+
+public struct StructTest
+{
+    // TODO: Uncommenting this makes Assembly.ExportedTypes throw
+    public static readonly StructTest? ReadOnlyField = null;
+    
+    // TODO: Uncommenting this makes Assembly.ExportedTypes throw
+    // public static StructTest? Field = new StructTest();
+    
+    // TODO: Uncommenting this makes Assembly.ExportedTypes throw
+    // public static StructTest? Property { get; set; } = null;
+}
+
+public class StructTestClass
+{
+    // This is fine
+    public static readonly StructTest? ReadOnlyField = null;
+    
+    // This is fine
+    public static StructTest? Field = new StructTest();
+    
+    // This is fine
+    public static StructTest? Property { get; set; } = null;
+}
+
+public struct ValueTypeTest
+{
+    // This is fine
+    public static readonly int? ReadOnlyField = null;
+
+    // This is fine
+    public static int? Field = 0;
+    
+    // This is fine
+    public static int? Property { get; set; } = null;
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ExportedTypesVsNullableStructs
+
+Demonstrates an issue with .NET's Assembly.ExportedTypes when using nullable struct members within a struct.
+To reproduce the issue, just call `dotnet run`.
